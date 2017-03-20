d4c7d8ded72a75d11d04d7523c5eb3d234b6bdae
SWEE JIN LIAN <SWEEJINLIAN@GMAIL.com>
2017-03-20 14:04:35 +0800

    Added validations and shorten method to url model; copied from bitly-clone app

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,17 @@
 class Url < ApplicationRecord
+
+  validates :long_url,
+      :uniqueness => true,
+      :presence => true,
+      :format => { :with => /http(s)*:\D\D\w+(.)\w+(.\D)*/}
+
+  validates :short_url,
+      :uniqueness => true
+
+  before_create :shorten
+
+  def shorten
+      self.short_url = SecureRandom.hex(4)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'urls#index'
-
   resources :urls
 
+  root 'urls#index'
+
   get "/:short_url", to: "urls#sweejin", as: "sj"
-  
+
 end
