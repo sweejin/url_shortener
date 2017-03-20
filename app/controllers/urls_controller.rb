f47c044b6a2ce49814cdecbf19222df058626e1b
SWEE JIN LIAN <SWEEJINLIAN@GMAIL.com>
2017-03-20 12:24:09 +0800

    SY corrected to enable routing error

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,8 +5,14 @@ class UrlsController < ApplicationController
   end
 
   def show
-    byebug 
+    byebug
     @url = Url.find(params[:id])
+  end
+
+  def sweejin
+    @url = Url.find_by(short_url: params[:short_url])
+    redirect_to @url.long_url
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'urls#index' 
+  root 'urls#index'
 
   resources :urls
+
+  get "/:short_url", to: "urls#sweejin", as: "sj"
+  
 end
